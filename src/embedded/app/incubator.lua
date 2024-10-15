172dcf4cfc396a14529f72be89f36ca254a55edb
Jere Castro <jerecastro2012@gmail.com>
2024-10-15 16:36:05 -0300

    Add humidifier update

--- src/embedded/app/incubator.lua
+++ src/embedded/app/incubator.lua
@@ -20,7 +20,7 @@ local M = {
 	resistor               = false,
 	humidifier             = false,
 	rotation               = false,
-	temperature            = 99.9, -- integer value of temperature [0.01 C]
+	temperature            = 99.9, -- integer value of temperature [99.9 C]
 	pressure               = 0,   -- integer value of preassure [Pa]=[0.01 hPa]
 	humidity               = 0,   -- integer value of rel.humidity [0.01 %]
 	is_testing             = false,
@@ -29,16 +29,22 @@ local M = {
 	is_sensorok            = false,
 	is_simulate_temp_local = false,
 	rotation_duration      = 5000, -- time in ms
-	rotation_period        = 3000,--3600000, -- time in ms
+	rotation_period        = 3600000, -- time in ms
 	humidifier_enabled     = true,
 	max_hum                = 70,
 	min_hum                = 60,
-	humidifier_max_on_time = 6, --sec
+	humidifier_max_on_time = 2, --sec	-- menor a Timer read_and_control() para que no corra 2 periodos
 	humidifier_off_time    = 15, -- sec
 	hum_turn_on_time       = 0,
 	hum_turn_off_time      = 0,
-	rotate_up			   = true
+	tray_one_date = 0,
+	tray_two_date = 0,
+	tray_three_date = 0,
+	incubation_period = 0,
+	hash = 1235,
+	incubator_name = "incubator_1"
 
+	
 }
 
 _G[M.name] = M
@@ -55,26 +61,18 @@ end
 
 function M.init_values()
 	M.startbme()
-	gpio.config({ gpio = { GPIORESISTOR, GPIOVOLTEO_UP, GPIOVOLTEO_DOWN, GPIOHUMID, GPIOVOLTEO_EN}, dir = gpio.OUT })
-	-- config inputs 
-  gpio.config( { gpio={GPIOREEDS_DOWN,GPIOREEDS_UP}, dir=gpio.IN,pull = gpio.PULL_UP})
+	gpio.config({ gpio = { GPIORESISTOR, GPIOVOLTEO_O1, GPIOVOLTEO_O2, GPIOHUMID }, dir = gpio.OUT })
 
-	gpio.set_drive(GPIOVOLTEO_UP, gpio.DRIVE_3)
-	gpio.set_drive(GPIOVOLTEO_DOWN, gpio.DRIVE_3)
+	gpio.set_drive(GPIOVOLTEO_O1, gpio.DRIVE_3)
+	gpio.set_drive(GPIOVOLTEO_O2, gpio.DRIVE_3)
 	gpio.set_drive(GPIOHUMID, gpio.DRIVE_3)
 	gpio.set_drive(GPIORESISTOR, gpio.DRIVE_3)
-	gpio.set_drive(GPIOVOLTEO_EN, gpio.DRIVE_3)
-
-	
 	
 	--revisar estos valores inicliales
-	gpio.write(GPIOVOLTEO_UP, 1)
-	gpio.write(GPIOVOLTEO_DOWN, 1)
+	gpio.write(GPIOVOLTEO_O1, 1)
+	gpio.write(GPIOVOLTEO_O2, 1)
 	gpio.write(GPIOHUMID, 1)
 	gpio.write(GPIORESISTOR, 1)
-	gpio.write(GPIOVOLTEO_EN, 0)
-
-	
 end -- end function
 
 -------------------------------------
@@ -182,32 +180,8 @@ function M.humidifier_switch(status)
 	local current_time = M.get_uptime_in_sec()
 	log.warn("humidifier current_time ".. current_time)
 
-	if M.humidifier_enabled then
-		log.warn("humidifier enabled")
-		if status then -- encender humidifier
-			if (not M.humidifier) then -- estaba apagado
-				log.warn("humidifier was off... turning on ")
-
-				--estaba apagado y lo prendo
-				M.hum_turn_on_time = current_time
-				M.humidifier = status
-			else
-				--estaba pendido y sigue
-				log.warn("humidifier was on... turned on" .. M.hum_turn_on_time)
-
-				log.warn("humidifier was on... turning on.. time transcurred " .. (current_time - M.hum_turn_on_time))
-				log.warn("humidifier was on... turning on.. time left " ..
-				(M.humidifier_max_on_time - (current_time - M.hum_turn_on_time)))
-				--verificar el tiempo maximo
-				if ((current_time - M.hum_turn_on_time) > M.humidifier_max_on_time) then
-					M.humidifier_enabled = false
-					M.humidifier = false
-					M.hum_turn_off_time = current_time
-					log.error("humidifier disabled beacuse time greater than max")
-				end
-			end
-		end
-	else --humidifier disabled
+	if not M.humidifier_enabled then
+		--humidifier disabled, check for waiting_off time concluded
 		log.error("humidifier disabled ")
 		if ((current_time - M.hum_turn_off_time) > M.humidifier_off_time) then
 			M.humidifier_enabled = true
@@ -223,12 +197,41 @@ function M.humidifier_switch(status)
 		end
 	end
 
+	log.warn("humidifier enabled")
+	if status and M.humidifier_enabled then -- encender humidifier
+		if (not M.humidifier) then -- estaba apagado
+			log.warn("humidifier was off... turning on ")
+
+			--estaba apagado y lo prendo
+			M.hum_turn_on_time = current_time
+			M.humidifier = status
+		else
+			--estaba pendido y sigue
+			log.warn("humidifier was on... turned on" .. M.hum_turn_on_time)
+
+			log.warn("humidifier was on... turning on.. time transcurred " .. (current_time - M.hum_turn_on_time))
+			log.warn("humidifier was on... turning on.. time left " ..
+			(M.humidifier_max_on_time - (current_time - M.hum_turn_on_time)))
+			--verificar el tiempo maximo de on
+			if ((current_time - M.hum_turn_on_time) > M.humidifier_max_on_time) then
+				M.humidifier_enabled = false
+				M.humidifier = false
+				M.hum_turn_off_time = current_time
+				log.error("humidifier disabled beacuse time greater than max")
+			end
+		end
+	end
+
 	if status and M.humidifier_enabled then
-		gpio.write(GPIOHUMID, 1)
+		-- logica negada
+		-- gpio.write(GPIOHUMID, 1)
+		gpio.write(GPIOHUMID, 0)
 		log.warn("humidifier pin turned on--------------------")
 	else
 		M.humidifier = false
-		gpio.write(GPIOHUMID, 0)
+		-- logica negada
+		-- gpio.write(GPIOHUMID, 0)
+		gpio.write(GPIOHUMID, 1)
 		log.warn("humidifier pin turned off--------------------")
 
 	end -- if end
@@ -242,38 +245,12 @@ end  -- function end
 function M.rotation_switch(status)
 	M.rotation = status
 	if status then
-		--switch on
-		M.rotation_change_dir()
-		if M.rotate_up then
-			gpio.write(GPIOVOLTEO_UP, 0)
-			gpio.write(GPIOVOLTEO_DOWN, 1)
-		else
-			gpio.write(GPIOVOLTEO_UP, 1)
-			gpio.write(GPIOVOLTEO_DOWN, 0)
-		end 
-		gpio.write(GPIOVOLTEO_EN,1)
+		gpio.write(GPIOVOLTEO_O1, 0)
 	else
-		--switch off 
-		gpio.write(GPIOVOLTEO_UP, 0)
-		gpio.write(GPIOVOLTEO_DOWN, 0)
-		gpio.write(GPIOVOLTEO_EN,0)
-	end
+		gpio.write(GPIOVOLTEO_O1, 1)
+	end -- if end
+	--todo: implement logger for debug
 end  -- function end
-
-function M.rotation_change_dir()
-	local upvalue = gpio.read(GPIOREEDS_UP)
-	local downvalue = gpio.read(GPIOREEDS_DOWN)
-	if (upvalue == 0 and downvalue == 1) then
-		M.rotate_up = false
-	elseif (upvalue == 1 and downvalue == 0) then
-		M.rotate_up = true
-	else
-		--something is wrong, invert rotation
-		log.error ("gpio reeds not active inverting rotation just in case")
-		M.rotate_up = 	not	M.rotate_up
-	end
-end
-
 
 -------------------------------------
 -- @function set_max_temp	modify the actual max_temp from API
@@ -289,8 +266,8 @@ function M.set_max_temp(new_max_temp)
 		return true
 	else
 		return false
-	end
-end
+	end -- if end
+end -- function end
 
 -------------------------------------
 -- @function set_min_temp	modify the actual min_temp from API
@@ -306,8 +283,8 @@ function M.set_min_temp(new_min_temp)
 		return true
 	else
 		return false
-	end
-end
+	end -- if end 
+end -- function end
 
 -------------------------------------
 -- @function set_rotation_period	modify the actual period time from API
@@ -322,8 +299,8 @@ function M.set_rotation_period(new_period_time)
 		return true
 	else
 		return false
-	end
-end
+	end -- if end
+end -- function end 
 
 -------------------------------------
 -- @function set_rotation_duration	modify the actual duration time from API
@@ -338,9 +315,106 @@ function M.set_rotation_duration(new_rotation_duration)
 		return true
 	else
 		return false
+	end -- if end
+end --function end 
+
+local tray_map = {
+	one = "tray_one_date",
+	two = "tray_two_date",
+	three = "tray_three_date"
+}
+---------------------------------------------------------------------------------------------------
+-- @function set_tray_date
+-- Ensures that new_tray_date is exactly a string of 10 digits before setting the value.
+-- @param tray_number string: "one", "two", or "three"
+-- @param new_tray_date number: Unix Time format
+-- @return boolean: true if the date was set successfully, false otherwise
+---------------------------------------------------------------------------------------------------
+function M.set_tray_date(tray_number, new_tray_date)
+	if type(new_tray_date) == "number" and #tostring(new_tray_date) == 10 then
+			local tray_var = tray_map[tray_number]
+			if tray_var then
+					M[tray_var] = new_tray_date
+					return true
+			end
+	end
+	return false
+end
+-------------------------------------------------------------------------------------------------
+-- @function set_incubation_period
+-- Ensures that new_tray_one_date is exactly a string of 10 digits before setting the value.
+-- @param	new_tray_*_date <-- Unix Time format
+-------------------------------------------------------------------------------------------------
+
+function M.set_incubation_period(new_incubation_period)
+	if type(new_incubation_period) == "number" and #tostring(new_incubation_period) == 10 then
+		M.incubation_period = new_incubation_period
+		return true
+	else
+		return false
+	end -- if end
+end     -- function end
+
+-------------------------------------------------------------------------------------------------
+-- @function set_hash
+-- varifies if the input string is at most 20 characters long and sets it as the hash if valid.
+-- @param	new_hash
+-------------------------------------------------------------------------------------------------
+
+function M.set_hash(new_hash)
+	if type(new_hash) == "string" and #new_hash <= 20 then
+		M.hash = new_hash
+		return true
+	else
+		return false
+	end -- if end
+end     -- function end
+
+-------------------------------------------------------------------------------------------------
+-- @function set_incubator_name
+-- set the name oif incubator 
+-- @param	new_incubator_name
+-------------------------------------------------------------------------------------------------
+function M.set_incubator_name(new_incubator_name)
+if type(new_incubator_name) == "string" and #new_incubator_name <= 20 then
+		M.incubator_name = new_incubator_name
+		return true
+else
+		return false
+	end -- if end 
+end -- function end
+
+-------------------------------------------------------------------------------------------------
+-- @function set_max_humidity
+-- set the max humidity parameter 
+-- @param	new_max_hum
+-------------------------------------------------------------------------------------------------
+function M.set_max_humidity(new_max_hum)
+	if new_max_hum ~= nil and new_max_hum >= 0
+			and new_max_hum > M.min_hum
+			and tostring(new_max_hum):sub(1, 1) ~= '-'
+			and type(new_max_hum) == "number" then
+		M.max_hum = new_max_hum
+		return true
+	else
+		return false
 	end
 end
-
-
+-------------------------------------------------------------------------------------------------
+-- @function set_min_humidity
+-- set the min humidity parameter 
+-- @param	new_min_hum
+-------------------------------------------------------------------------------------------------
+function M.set_min_humidity(new_min_hum)
+	if new_min_hum ~= nil and new_min_hum >= 0
+		and new_min_hum < M.max_hum
+		and tostring(new_min_hum):sub(1, 1) ~= '-'
+		and type(new_min_hum) == "number" then
+	M.min_hum = new_min_hum
+			return true
+		else
+			return false
+		end
+end
 
 return M
