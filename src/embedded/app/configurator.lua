3f9b412e0b81581dd7cb504357f28a948425b43e
Jere Castro <jerecastro2012@gmail.com>
2024-12-20 06:46:46 -0300

    Add ntfy messages as error in logs

--- src/embedded/app/configurator.lua
+++ src/embedded/app/configurator.lua
@@ -1,20 +1,23 @@
 configurator =
 {
-  incubator = {},
-  WiFi = require("wifiinit"),
+	incubator = {},
+	WiFi = require("wifiinit"),
+	log = require("log")
 
 }
 -------------------------------------------------------------------------------------
 -- @method configurator:init_module   load config file to incubator table
 -------------------------------------------------------------------------------------
 function configurator:init_module(incubator_object)
-  local config_table = configurator:read_config_file()
-  configurator.incubator = incubator_object
-  if config_table ~= nil then
-    configurator:load_objects_data(config_table)
-  end
+	local config_table = configurator:read_config_file()
+	configurator.incubator = incubator_object
+	if config_table ~= nil then
+		configurator:load_objects_data(config_table)
+		if config_table.hash then
+			configurator.log.set_ntfy_url(config_table.hash)
+		end
+	end
 end
-
 
 -------------------------------------------------------------------------------------
 --------------------------        CONFIGURATOR       --------------------------------
@@ -25,15 +28,15 @@ end
 -- @method configurator:encode_config_file	encode the new config.json
 -------------------------------------------------------------------------------------
 function configurator:encode_config_file(new_config_table)
-  local table_to_json = sjson.encode(new_config_table)
-  local new_config_file = io.open("config.json", "w")
-  if not new_config_file then
-    return false
-  else
-    new_config_file:write(table_to_json)
-    new_config_file:close()
-    return true
-  end -- if else end
+	local table_to_json = sjson.encode(new_config_table)
+	local new_config_file = io.open("config.json", "w")
+	if not new_config_file then
+		return false
+	else
+		new_config_file:write(table_to_json)
+		new_config_file:close()
+		return true
+	end -- if else end
 end
 
 -------------------------------------------------------------------------------------
@@ -43,22 +46,22 @@ end
 function configurator:create_config_file()
 	log.trace("Creating a new config file")
 	local new_file = io.open("config.json", "w")
-			local config = {
-					rotation_duration = 50000,
-					rotation_period = 3600000,
-					min_temperature = 37.3,
-					max_temperature = 37.8,
-					tray_one_date = 0,
-					tray_two_date = 0,
-					tray_three_date = 0,
-					incubation_period = 0,
-					hash = "1234567890",
-					incubator_name = string.format("incubadora-%s",wifi.sta.getmac()),
-					max_hum = 70,
-					min_hum = 60
-			}
-			new_file:write(sjson.encode(config))
-			new_file:close()
+	local config = {
+		rotation_duration = 5000,
+		rotation_period = 3600000,
+		min_temperature = 37.3,
+		max_temperature = 37.8,
+		tray_one_date = 0,
+		tray_two_date = 0,
+		tray_three_date = 0,
+		incubation_period = 0,
+		hash = "incubator_parameters",
+		incubator_name = string.format("incubadora-%s", wifi.sta.getmac()),
+		max_hum = 70,
+		min_hum = 60
+	}
+	new_file:write(sjson.encode(config))
+	new_file:close()
 end
 
 -------------------------------------------------------------------------------------
@@ -66,15 +69,15 @@ end
 -------------------------------------------------------------------------------------
 
 function configurator:read_config_file()
-  local file = io.open("config.json", "r")
-  if not file then
-    configurator:create_config_file()
-    file = io.open("config.json", "r")
-  end
-    local config_json = file:read("*a")
-    file:close()
-    local config_table = sjson.decode(config_json)
-    return config_table
+	local file = io.open("config.json", "r")
+	if not file then
+		configurator:create_config_file()
+		file = io.open("config.json", "r")
+	end
+	local config_json = file:read("*a")
+	file:close()
+	local config_table = sjson.decode(config_json)
+	return config_table
 end
 
 -----------------------------------------------------------------------------------
@@ -84,38 +87,38 @@ end
 -- @ param new_config_table
 -----------------------------------------------------------------------------------
 function configurator:load_objects_data(new_config_table)
-  local status = {}
+	local status = {}
 
-  for param, value in pairs(new_config_table) do
-    if param == "min_temperature" then
-      status.min_temp = incubator.set_min_temp(tonumber(value))
-    elseif param == "max_temperature" then
-      status.max_temp = incubator.set_max_temp(tonumber(value))
-    elseif param == "rotation_duration" then
-      status.rotation_duration = incubator.set_rotation_duration(tonumber(value))
-    elseif param == "rotation_period" then
-      status.rotation_period = incubator.set_rotation_period(tonumber(value))
+	for param, value in pairs(new_config_table) do
+		if param == "min_temperature" then
+			status.min_temp = incubator.set_min_temp(tonumber(value))
+		elseif param == "max_temperature" then
+			status.max_temp = incubator.set_max_temp(tonumber(value))
+		elseif param == "rotation_duration" then
+			status.rotation_duration = incubator.set_rotation_duration(tonumber(value))
+		elseif param == "rotation_period" then
+			status.rotation_period = incubator.set_rotation_period(tonumber(value))
 		elseif param == "tray_one_date" then
-			status.tray_one_date = incubator.set_tray_date("one",tonumber(value))
+			status.tray_one_date = incubator.set_tray_date("one", tonumber(value))
 		elseif param == "tray_two_date" then
-			status.tray_two_date = incubator.set_tray_date("two",tonumber(value))
+			status.tray_two_date = incubator.set_tray_date("two", tonumber(value))
 		elseif param == "tray_three_date" then
-			status.tray_three_date = incubator.set_tray_date("three",tonumber(value))
-		elseif param == "incubation_period" then 
+			status.tray_three_date = incubator.set_tray_date("three", tonumber(value))
+		elseif param == "incubation_period" then
 			status.incubation_period = incubator.set_incubation_period(tonumber(value))
-		elseif param == "hash" then 
+		elseif param == "hash" then
 			status.hash = incubator.set_hash(value)
-    elseif param == "incubator_name" then
+			configurator.log.set_ntfy_url(value)
+		elseif param == "incubator_name" then
 			status.incubator_name = incubator.set_incubator_name(tostring(value))
-		elseif param == "max_hum" then 
+		elseif param == "max_hum" then
 			status.max_hum = incubator.set_max_humidity(tonumber(value))
 		elseif param == "min_hum" then
 			status.min_hum = incubator.set_min_humidity(tonumber(value))
-		end -- if end 
-
-  end -- for end
+		end -- if end
+	end -- for end
 	configurator.WiFi:on_change(new_config_table)
-  return status
-end -- function end 
+	return status
+end -- function end
 
 return configurator
