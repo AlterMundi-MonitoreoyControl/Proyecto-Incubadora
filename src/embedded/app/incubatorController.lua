4bda8f46ef926c913301084157c5f097dc568602
javierbrk <javierbrk@gmail.com>
2025-08-21 10:58:35 -0300

    fix

--- src/embedded/app/incubatorController.lua
+++ src/embedded/app/incubatorController.lua
@@ -81,7 +81,7 @@ function temp_control(temperature, min_temp, max_temp)
 
     if temperature > max_temp + 4 then
         log.addError("temperature", "[T] temperature too high: " .. temperature)
-    else if temperature < min_temp - 10 then
+    elseif temperature < min_temp - 10 then
         log.addError("temperature", "[T] temperature too low: " .. temperature)   
     end
 
